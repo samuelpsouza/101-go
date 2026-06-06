87e50702f0cebdcfe6f33cc787c990b1d1b54f84
Samuel Souza <samuelsouza.contact@gmail.com>
2026-06-06 13:25:45 -0300

    feat(static_array): implement unit tests for constructor

--- DataStructures/array/static_array_test.go
+++ DataStructures/array/static_array_test.go
@@ -4,7 +4,11 @@ import (
 	"testing"
 )
 
-func TestEmptyUnsortedArray(t *testing.T) {
+func TestUnsortedArray(t *testing.T) {
+	t.Run("NewUnsortedArray", testNewUnsortedArray)
+}
+
+func testNewUnsortedArray(t *testing.T) {
 	tests := []struct {
 		name    string
 		maxSize int
@@ -20,6 +24,14 @@ func TestEmptyUnsortedArray(t *testing.T) {
 			arr := NewUnsortedArray[int](tt.maxSize)
 			if arr.MaxSize != tt.maxSize {
 				t.Errorf("Expected MaxSize=%d, got %d", tt.maxSize, arr.MaxSize)
+			}
+
+			if arr.Size != 0 {
+				t.Errorf("Expected Size=0, got %d", arr.Size)
+			}
+
+			if len(arr.Array) != tt.maxSize {
+				t.Errorf("Expected underlying array length=%d, got %d", tt.maxSize, len(arr.Array))
 			}
 		})
 	}
